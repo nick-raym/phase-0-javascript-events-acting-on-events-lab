a86517324dbf0ac4dcac2078157100bf63f68455
NickRayman <nlhrayman@gmail.com>
2023-12-21 18:56:04 -0500

    tests don't pass but everything runs correctly

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function moveDodgerLeft(){
+const dodger=document.getElementById("dodger");
+dodger.style.backgroundColor="rgb(255 0 86)";
+
+document.addEventListener('keydown',function(event){
+    if(event.key==="ArrowLeft"){
+        const unpx = dodger.style.left.replace("px","");
+        const pars=parseInt(unpx,10);
+        if(pars>0){
+        dodger.style.left=`${pars-1}px`;
+        console.log("DODGER moved left:", dodger.style.left);
+    }
+        
+    }
+})
+}
+
+moveDodgerLeft();
+
+function moveDodgerRight(){
+
+    document.addEventListener('keydown',function(event){
+        if(event.key==="ArrowRight"){
+            const unpx = dodger.style.left.replace("px","");
+            const pars=parseInt(unpx,10);
+            if(pars<360){
+            dodger.style.left=`${pars+1}px`;
+            console.log("DODGER moved right:", dodger.style.left);
+        }
+            
+        }
+    })
+}
+
+moveDodgerRight();
